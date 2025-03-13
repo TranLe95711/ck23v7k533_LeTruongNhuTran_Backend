eb7007532712761ac3ab351838203b86e42ad33c
TranLe95711 <kopachop@gmail.com>
2025-03-14 01:22:34 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
+app.use("/api/contacts", contactsRouter);
 // handle 404 response
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -18,11 +25,4 @@ app.use((err, req, res, next) => {
         message: err.message || "Internal Server Error",
     });
 });
-app.use(cors());
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
-});
-app.use("/api/contacts", contactsRouter);
-
 module.exports = app;
